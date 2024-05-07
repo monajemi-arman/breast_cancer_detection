b8b6f999cf4d0f72f980a53f06405f90d8cda71d
monajemia <watbut67@gmail.com>
2024-05-07 20:37:10 +0330

    Incomplete JSON creation. Single class. Going for multi class support in next commit.

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-# Dataset loader
+# Convert dataset DICOM and XML to images and masks directory
 import os
+import time
 import re
 from pathlib import Path
 import ast
@@ -9,13 +10,19 @@ import pydicom
 import xmltodict
 import numpy as np
 from PIL import Image
+import json
 
 # --- Parameters --- #
 # Output paths
 image_out_dir = 'images'
 mask_out_dir = 'masks'
+json_out = 'annotations.json'
+json_or_mask_choice = 'json'.lower()  # json/mask/both
 # Classes chosen for segmentation
 chosen_classes = ['Mass']
+
+# Overall Counters for ID
+image_id = 0
 
 # INBreast Dataset
 # Dir paths
@@ -25,15 +32,30 @@ inbreast_dcm_dir = os.path.join(inbreast_path, 'AllDICOMs')
 
 # --- End of Parameters --- #
 
-# Create output dirs
+# Create output json and/or dirs
+if json_or_mask_choice == 'json':
+    mask_out_dir = None
+    json_data = {
+        'categories': [],
+        'annotations': [],
+        'images': []
+    }
+    # Prepare JSON categories
+    for cls in chosen_classes:
+        json_data['categories'].append({
+            'id': chosen_classes.index(cls) + 1,
+            'name': cls
+        })
+
 for directory in [image_out_dir, mask_out_dir]:
-    if not os.path.isdir(directory):
+    if directory and not os.path.isdir(directory):
         os.mkdir(directory)
 
 inbreast_classes = set()
 inbreast_xmls = [str(x) for x in list(Path(inbreast_xml_dir).glob('*.xml'))]  # Load XML paths
 
 for inbreast_xml in inbreast_xmls:
+    image_id += 1
     # Read XML to Dict
     with open(inbreast_xml) as f:
         xml_data = f.read()
@@ -71,9 +93,38 @@ for inbreast_xml in inbreast_xmls:
         # Read pixels from DICOM, convert tp 0-255 range for JPEG
         pixel_array = pydicom.read_file(dcm_path).pixel_array.astype(np.uint8)
         image = Image.fromarray(pixel_array)
-        image.save(os.path.join(image_out_dir, dcm_prefix + '.jpg'), format='JPEG')
-        # Create mask
-        mask = np.zeros(pixel_array.shape, dtype=np.uint8)
-        mask = cv2.fillPoly(mask, rois, 255)
-        mask = Image.fromarray(mask)
-        mask.save(os.path.join(mask_out_dir, dcm_prefix + '.png'), format='PNG')
+        jpeg_path = os.path.join(image_out_dir, dcm_prefix + '.jpg')
+        if not os.path.exists(jpeg_path):
+            image.save(jpeg_path, format='JPEG')
+        if json_or_mask_choice != 'json':
+            # Create mask
+            mask = np.zeros(pixel_array.shape, dtype=np.uint8)
+            mask = cv2.fillPoly(mask, rois, 255)
+            mask = Image.fromarray(mask)
+            mask.save(os.path.join(mask_out_dir, dcm_prefix + '.png'), format='PNG')
+        if json_or_mask_choice != 'mask':
+            # Get image creation date
+            image_date = time.gmtime(os.path.getctime(jpeg_path))
+            image_date = '{}/{}/{} {}:{}:{}'.format(image_date.tm_year, image_date.tm_mon, image_date.tm_mday,
+                                                    image_date.tm_hour, image_date.tm_min, image_date.tm_sec)
+            # Add image to JSON
+            json_data['images'].append({
+                'id': image_id,
+                'file_name': jpeg_path,
+                'width': image.width,
+                'height': image.height,
+                'date_captured': image_date
+            })
+            # Add annotations to JSON
+            for roi in rois:
+                json_data['annotations'].append({
+                    'id': annotation_id,
+                    'image_id': image_id,
+                    'category_id': None,
+                    'segmentation': roi,
+                    'bbox': None
+                })
+
+if json_data:
+    with open(json_out, 'w') as f:
+        json.dump(json_data, f)
