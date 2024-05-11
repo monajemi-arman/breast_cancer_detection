3daa27ec86fb47afa0b9f086d23a3cbc7940067b
monajemia <watbut67@gmail.com>
2024-05-11 18:40:50 +0330

    Added YOLO dataset output support

--- convert_dataset.py
+++ convert_dataset.py
@@ -15,9 +15,10 @@ import json
 # --- Parameters --- #
 # Output paths
 image_out_dir = 'images'
-mask_out_dir = 'masks'
-json_out = 'annotations.json'
-json_or_mask_choice = 'json'.lower()  # json/mask/both
+mask_out_dir = 'masks' # Mask
+json_out = 'annotations.json' # COCO
+txt_out_dir = 'labels' # Yolo
+output_choice = 'yolo'  # yolo/coco/mask
 # Classes chosen for segmentation
 chosen_classes = ['Mass']
 
@@ -33,8 +34,12 @@ inbreast_dcm_dir = os.path.join(inbreast_path, 'AllDICOMs')
 # --- End of Parameters --- #
 
 # Create output json and/or dirs
-if json_or_mask_choice == 'json':
+output_choice = output_choice.lower()
+json_data = None # Prevent undefined error
+if output_choice == 'coco':
+    # Prevent unnecessary folders being created
     mask_out_dir = None
+    txt_out_dir = None
     json_data = {
         'categories': [],
         'annotations': [],
@@ -46,8 +51,10 @@ if json_or_mask_choice == 'json':
             'id': chosen_classes.index(cls) + 1,
             'name': cls
         })
+if output_choice == 'yolo':
+    mask_out_dir = None
 
-for directory in [image_out_dir, mask_out_dir]:
+for directory in [image_out_dir, mask_out_dir, txt_out_dir]:
     if directory and not os.path.isdir(directory):
         os.mkdir(directory)
 
@@ -99,42 +106,62 @@ for inbreast_xml in inbreast_xmls:
         jpeg_path = os.path.join(image_out_dir, dcm_prefix + '.jpg')
         if not os.path.exists(jpeg_path):
             image.save(jpeg_path, format='JPEG')
-        if json_or_mask_choice != 'json':
-            # Create mask
+        if output_choice == 'mask':
+            # Mask mode
             mask = np.zeros(pixel_array.shape, dtype=np.uint8)
             for cls in chosen_classes:
                 mask = cv2.fillPoly(mask, rois[cls], 255 - chosen_classes.index(cls))
             mask = Image.fromarray(mask)
             mask.save(os.path.join(mask_out_dir, dcm_prefix + '.png'), format='PNG')
-        if json_or_mask_choice != 'mask':
-            # Get image creation date
-            image_date = time.gmtime(os.path.getctime(jpeg_path))
-            image_date = '{}/{}/{} {}:{}:{}'.format(image_date.tm_year, image_date.tm_mon, image_date.tm_mday,
-                                                    image_date.tm_hour, image_date.tm_min, image_date.tm_sec)
-            # Add image to JSON
-            json_data['images'].append({
-                'id': image_id,
-                'file_name': jpeg_path,
-                'width': image.width,
-                'height': image.height,
-                'date_captured': image_date
-            })
-            # Add annotations to JSON
+        if output_choice == 'coco' or output_choice == 'yolo':
+            if output_choice == 'coco':
+                # Get image creation date
+                image_date = time.gmtime(os.path.getctime(jpeg_path))
+                image_date = '{}/{}/{} {}:{}:{}'.format(image_date.tm_year, image_date.tm_mon, image_date.tm_mday,
+                                                        image_date.tm_hour, image_date.tm_min, image_date.tm_sec)
+                # Add image to JSON
+                json_data['images'].append({
+                    'id': image_id,
+                    'file_name': jpeg_path,
+                    'width': image.width,
+                    'height': image.height,
+                    'date_captured': image_date
+                })
+            # Add annotations with YOLO or COCO style
             for cls in rois.keys():
+                class_id = chosen_classes.index(cls)
+                txt_lines = [] # For YOLO
                 for roi in rois[cls]:
-                    # Calculate bounding box
-                    roi = roi.astype(int)
-                    x_s, y_s = roi[:, 0], roi[:, 1]
-                    bbox = np.array([x_s.min(), y_s.min(), x_s.max(), y_s.max()]).tolist()
-                    # Bug: Currently, we are not taking into account the polygons with disconnected parts.
-                    # They may not be present in INBreast dataset, however, it is something that we could not
-                    # take into account.
-                    json_data['annotations'].append({
-                        'image_id': image_id,
-                        'category_id': chosen_classes.index(cls),
-                        'segmentation': [roi.tolist()],
-                        'bbox': bbox
-                    })
+                    if output_choice == 'coco':
+                        # COCO mode
+                        # Bug: Currently, we are not taking into account the polygons with disconnected parts.
+                        # They may not be present in INBreast dataset, however, it is something that we could not
+                        # take into account.
+                        # ---
+                        # Calculate bounding box
+                        roi = roi.astype(int)
+                        x_s, y_s = roi[:, 0], roi[:, 1]
+                        # Convert all data to simple list of simple integers
+                        bbox = np.array([x_s.min(), y_s.min(), x_s.max(), y_s.max()]).tolist()
+                        json_data['annotations'].append({
+                            'image_id': image_id,
+                            'category_id': class_id,
+                            'segmentation': [roi.tolist()],
+                            'bbox': bbox
+                        })
+                    elif output_choice == 'yolo':
+                        # YOLO mode
+                        # Relative Xs and Ys
+                        x_s, y_s = roi[:, 0] / image.width, roi[:, 1] / image.height
+                        # BBOX in YOLO: X-Center Y-Center Width Height
+                        bbox = x_s.mean(), y_s.mean(), x_s.max() - x_s.min(), y_s.max() - y_s.min()
+                        bbox = [str(x) for x in bbox]
+                        txt_lines.append("{} {} {} {} {}\n".format(str(class_id), *bbox))
+    # If YOLO, write TXT labels accumulated for the current image
+    if txt_lines and len(txt_lines) > 0:
+        with open(os.path.join(txt_out_dir, dcm_prefix + '.txt'), 'w') as f:
+            f.writelines(txt_lines)
+
 
 if json_data:
     with open(json_out, 'w') as f:
