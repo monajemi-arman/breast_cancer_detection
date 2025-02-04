2d69ecb525657fc97dd1f6ba8ab918bc6da34c3e
monajemia <watbut67@gmail.com>
2025-02-04 13:59:50 +0330

    move read_dicom to utils

--- custom_convert_dataset.py
+++ custom_convert_dataset.py
@@ -9,6 +9,7 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 from pydicom.pixel_data_handlers.util import apply_voi_lut
+from utils import read_dicom
 
 input_dataset_path = 'datasets/custom'
 output_dataset_path = 'custom-dataset'
@@ -138,39 +139,6 @@ def merge_dicts(dict1, dict2):
 def read_custom_dicom(file_path):
     image = read_dicom(file_path)
     image = ImageOps.invert(image)
-    return image
-
-
-def read_dicom(file_path):
-    dicom_data = pydicom.dcmread(file_path, force=True)
-    pixel_array = dicom_data.pixel_array.astype(np.float32)
-
-    rescale_slope = float(dicom_data.get("RescaleSlope", 1))
-    rescale_intercept = float(dicom_data.get("RescaleIntercept", 0))
-
-    # Handle MultiValue for WindowCenter and WindowWidth
-    window_center = dicom_data.get("WindowCenter", np.mean(pixel_array))
-    if isinstance(window_center, pydicom.multival.MultiValue):
-        window_center = float(window_center[0])
-    else:
-        window_center = float(window_center)
-
-    window_width = dicom_data.get("WindowWidth", np.max(pixel_array) - np.min(pixel_array))
-    if isinstance(window_width, pydicom.multival.MultiValue):
-        window_width = float(window_width[0])
-    else:
-        window_width = float(window_width)
-
-    hu_pixels = (pixel_array * rescale_slope) + rescale_intercept
-    min_window = window_center - (window_width / 2)
-    max_window = window_center + (window_width / 2)
-
-    normalized = np.clip((hu_pixels - min_window) / (max_window - min_window), 0, 1)
-
-    image_array = (normalized * 255).astype(np.uint8)
-
-    image = Image.fromarray(image_array)
-
     return image
 
 
--- utils.py
+++ utils.py
@@ -3,6 +3,42 @@ import json
 import os
 from PIL import Image
 import cv2
+import numpy as np
+import pydicom
+
+
+def read_dicom(file_path):
+    dicom_data = pydicom.dcmread(file_path, force=True)
+    pixel_array = dicom_data.pixel_array.astype(np.float32)
+
+    rescale_slope = float(dicom_data.get("RescaleSlope", 1))
+    rescale_intercept = float(dicom_data.get("RescaleIntercept", 0))
+
+    # Handle MultiValue for WindowCenter and WindowWidth
+    window_center = dicom_data.get("WindowCenter", np.mean(pixel_array))
+    if isinstance(window_center, pydicom.multival.MultiValue):
+        window_center = float(window_center[0])
+    else:
+        window_center = float(window_center)
+
+    window_width = dicom_data.get("WindowWidth", np.max(pixel_array) - np.min(pixel_array))
+    if isinstance(window_width, pydicom.multival.MultiValue):
+        window_width = float(window_width[0])
+    else:
+        window_width = float(window_width)
+
+    hu_pixels = (pixel_array * rescale_slope) + rescale_intercept
+    min_window = window_center - (window_width / 2)
+    max_window = window_center + (window_width / 2)
+
+    normalized = np.clip((hu_pixels - min_window) / (max_window - min_window), 0, 1)
+
+    image_array = (normalized * 255).astype(np.uint8)
+
+    image = Image.fromarray(image_array)
+
+    return image
+
 
 # YOLO to JSON Conversion
 def yolo_to_coco(yolo_annotations, image_dir, output_file):
